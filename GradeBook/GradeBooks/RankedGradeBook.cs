b7275fecafb3210d076d2d8871a91b48c1070088
philippe.roy <philippe.roy@carsales.com.au>
2019-05-01 12:34:30 +1000

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,9 +32,27 @@ namespace GradeBook.GradeBooks
             return 'F';
         }
 
-        private Exception InvalidOperationException()
+        public override void CalculateStatistics()
         {
-            throw new NotImplementedException();
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
